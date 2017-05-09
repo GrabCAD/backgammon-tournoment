d78d0c94d7e277864b0577515740a0a763ba6b23
Ariel Hadad <arielhad@gmail.com>
2017-05-09 16:20:37 +0300

    Add basic environment for implementing the backgammon tournament

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to your Backgammon Tournament</h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
